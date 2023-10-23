e90230f1ded32cdc22632732946764ff2f2d97b4
Yann.Vasseur <yannvasseur@reactor.fr>
2023-10-23 10:34:40 +0200

    add infrastructure test

---
+++ tests/Onyx.Infrastructure.Tests/WeatherForecastDataServicesTests.cs
@@ -0,0 +1,27 @@
+﻿using Onyx.Core.Interfaces;
+using Onyx.Infrastructure.Services;
+
+namespace Onyx.Infrastructure.Tests
+{
+    public class WeatherForecastDataServicesTests
+    {
+        private readonly IWeatherForecastDataServices _weatherForecastDataServices;
+
+        public WeatherForecastDataServicesTests()
+        {
+            _weatherForecastDataServices = new WeatherForecastDataServices();
+        }
+
+        [Fact]
+        public async Task GetAllWeatherForecasts_ShouldReturn_NoNull()
+        {
+            //Arrange
+
+            //Act
+            var results = await _weatherForecastDataServices.GetAllAsync();
+
+            //Assert
+            Assert.NotNull(results);
+        }
+    }
+}
